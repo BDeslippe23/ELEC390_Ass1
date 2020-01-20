56e416c8ffabd22f38af3fe3d9787311f8a983f9
BDeslippe23 <bdeslippe@outlook.com>
2020-01-20 16:25:33 -0500

    Fixed Profile Activity

--- app/src/main/java/com/example/elec390_ass1/MainActivity.java
+++ app/src/main/java/com/example/elec390_ass1/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.elec390_ass1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    protected static final String TAG = "MainActivity";
+
+    SharedPreferences sharedPref;
 
     Button button_goToGrades = null;
     Button button_goToProfile = null;
@@ -21,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupUI();
+
+        //Clears all preferences at startup.
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.clear();
+        editor.commit();
 
         button_goToGrades.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,25 +48,36 @@ public class MainActivity extends AppCompatActivity {
                 gotoProfileActivity();
             }
         }));
+
+        checkProfileName();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        checkProfileName();
     }
 
     protected void setupUI(){
         button_goToGrades = findViewById(R.id.button_goToGrades);
         button_goToProfile = findViewById(R.id.button_goToProfile);
     }
-
     protected void gotoGradeActivity(){
         Intent toGrades = new Intent(MainActivity.this,gradeActivity.class);
         startActivity(toGrades);
     }
-
     protected void gotoProfileActivity(){
         Intent toProfile = new Intent(MainActivity.this,profileActivity.class);
         startActivity(toProfile);
     }
-
-    void goToGradeActivity(){
-        Intent goToGradeActivity = new Intent(MainActivity.this, gradeActivity.class);
-        startActivity(goToGradeActivity);
+    protected void checkProfileName(){
+        sharedPref = getSharedPreferences(getString(R.string.profileFile), Context.MODE_PRIVATE);
+        String name = sharedPref.getString(getString(R.string.profileName),"");
+        Log.d(TAG,"Name = "+name);
+        if(name.equals("")){
+            Toast.makeText(getApplicationContext(),"You must create a profile",Toast.LENGTH_SHORT).show();
+            gotoProfileActivity();
+        }
     }
 }
--- app/src/main/java/com/example/elec390_ass1/profileActivity.java
+++ app/src/main/java/com/example/elec390_ass1/profileActivity.java
@@ -1,5 +1,7 @@
 package com.example.elec390_ass1;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +11,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -19,10 +23,19 @@ import android.widget.Toast;
 
 public class profileActivity extends AppCompatActivity {
 
+    protected static final String TAG = "profileActivity";
+
     EditText editText_name = null;
     EditText editText_age = null;
     EditText editText_studentID = null;
+
     Button button_saveProfile = null;
+
+    private String pName;
+    private String pAge;
+    private String pID;
+
+    SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +52,41 @@ public class profileActivity extends AppCompatActivity {
         button_saveProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO create save profile function
+                pName = editText_name.getText().toString();
+                pAge = editText_age.getText().toString();
+                pID = editText_studentID.getText().toString();
+
+                if(checkFields()){
+                    saveProfile();
+                }
+                disableEditText();
+                //Logging for personal use
+                /*
+                if(pName.equals("")){
+                    Log.d(TAG,"pName = empty");
+                }
+                if(pName == null){
+                    Log.d(TAG,"pName = null");
+                }
+
+                 */
             }
         });
 
+
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sharedPref = getSharedPreferences(getString(R.string.profileFile),Context.MODE_PRIVATE);
+        editText_name.setText(sharedPref.getString(getString(R.string.profileName),""));
+        editText_age.setText(sharedPref.getString(getString(R.string.profileAge),""));
+        editText_studentID.setText(sharedPref.getString(getString(R.string.profileID),""));
+    }
+
+    // Menu Inflator
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -56,24 +98,63 @@ public class profileActivity extends AppCompatActivity {
     //  This uses XML file from menu resource folder
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        //TODO make function to run here that enables editing profile fields.
         if(item.getItemId()==R.id.item_actionButton){
-            Toast.makeText(this,"Edit Profile Selected",Toast.LENGTH_LONG).show();
+            enableEditText();
+            Toast.makeText(this,"Edit Profile Selected",Toast.LENGTH_SHORT).show();
             return true;
         }
         else{
             return super.onOptionsItemSelected(item);
         }
     }
-
     protected void setupUI(){
         editText_age = findViewById(R.id.editText_age);
         editText_name = findViewById(R.id.editText_Name);
         editText_studentID = findViewById(R.id.editText_studentID);
         button_saveProfile = findViewById(R.id.button_saveProfile);
 
+        disableEditText();
+    }           //Initializes UI
+    protected void enableEditText(){
+        editText_studentID.setEnabled(true);
+        editText_name.setEnabled(true);
+        editText_age.setEnabled(true);
+    }    //Enables all edit text fields in this activity
+    protected void disableEditText(){
         editText_studentID.setEnabled(false);
         editText_name.setEnabled(false);
         editText_age.setEnabled(false);
+    }   //Disables all edit text fields in this activity
+
+
+    protected Boolean checkFields(){           //Checking if all the profile fields meet the requirements.
+        if(pName.equals("") || pAge.equals("") || pID.equals("")){
+            Toast.makeText(this,"all fields must be filled",Toast.LENGTH_SHORT).show();
+            return false;
+        } else if( Integer.parseInt(pAge)<18){
+            editText_age.setText("");
+            Log.d(TAG,"checkField Age = empty");
+            editText_age.requestFocus();
+            Toast.makeText(this,"Age limit not in range (18-99)",Toast.LENGTH_SHORT).show();
+            return false;
+        } else if( Integer.parseInt(pID)<100000){
+            editText_studentID.setText("");
+            Log.d(TAG,"checkField ID = empty");
+            editText_studentID.requestFocus();
+            Toast.makeText(this,"ID must be 8 digits and cannot start with 0",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else return true;
+    }
+
+    protected void saveProfile(){
+        sharedPref = getSharedPreferences(getString(R.string.profileFile), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString(getString(R.string.profileName),pName);
+        editor.putString(getString(R.string.profileAge),pAge);
+        editor.putString(getString(R.string.profileID),pID);
+        editor.apply();
+        Toast.makeText(getApplicationContext(),"Profile Saved",Toast.LENGTH_SHORT).show();
     }
 }
